afbb0820df6d2fc27cdeda2e56308c2927fb118c
rin <rin@debian.area51.gr.jp>
2018-04-29 16:11:04 +0900

    add getopt_long

--- 07/01/head.c
+++ 07/01/head.c
@@ -1,26 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define _GNU_SOURCE
+#include <getopt.h>
+
+#define DEFAULT_N_LINES 10
+
 static void do_head(FILE* f, long nlines);
+
+static struct option longopts[] =
+{
+	{"lines", required_argument, NULL, 'n'},
+	{"help",  no_argument,       NULL, 'h'},
+	{0, 0, 0, 0},
+};
+
 
 int main(int ac, char** av)
 {
-	long nlines;
+	int opt;
+	long nlines = DEFAULT_N_LINES;
 
-	if(ac < 2)
+	while((opt = getopt_long(ac, av, "n:", longopts, NULL)) != -1)
 	{
-		fprintf(stderr, "usage: %s n [file file...]\n", av[0]);
-		exit(1);
+		switch(opt)
+		{
+			case 'n':
+				nlines = atol(optarg);
+				break;
+			case 'h':
+				fprintf(stdout, "usage: %s n [file file...]\n", av[0]);
+				exit(1);
+				break;
+			case '?':
+				fprintf(stderr, "usage: %s n [file file...]\n", av[0]);
+				exit(1);
+				break;
+		}
 	}
-	nlines = atol(av[1]);
-	if(ac == 2)
+
+	if(optind == ac)
 	{
-		do_head(stdin, atol(av[1]));
+		do_head(stdin, nlines);
 	}
 	else
 	{
 		int i;
-		for(i = 2; i < ac; i++)
+		for(i = optind; i < ac; i++)
 		{
 			FILE* f;
 			if((f = fopen(av[i], "r")) == NULL)
