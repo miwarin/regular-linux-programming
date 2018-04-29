dac683980c360299ea297710e36ce90a90a233ea
rin <rin@debian.area51.gr.jp>
2018-04-29 15:49:42 +0900

    add file read

--- 07/01/head.c
+++ 07/01/head.c
@@ -5,12 +5,33 @@ static void do_head(FILE* f, long nlines);
 
 int main(int ac, char** av)
 {
-	if(ac != 2)
+	long nlines;
+
+	if(ac < 2)
 	{
-		fprintf(stderr, "usage: %s n\n", av[0]);
+		fprintf(stderr, "usage: %s n [file file...]\n", av[0]);
 		exit(1);
 	}
-	do_head(stdin, atol(av[1]));
+	nlines = atol(av[1]);
+	if(ac == 2)
+	{
+		do_head(stdin, atol(av[1]));
+	}
+	else
+	{
+		int i;
+		for(i = 2; i < ac; i++)
+		{
+			FILE* f;
+			if((f = fopen(av[i], "r")) == NULL)
+			{
+				perror(av[i]);
+				exit(1);
+			}
+			do_head(f, nlines);
+			fclose(f);
+		}
+	}
 	exit(0);
 }
 
