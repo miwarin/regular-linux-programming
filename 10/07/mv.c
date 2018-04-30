5258e1a966163e879c029945098760d1968f568a
rin <rin@debian.area51.gr.jp>
2018-04-30 16:56:13 +0900

    add mv.c

---
+++ 10/07/mv.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int ac, char** av)
+{
+	if(ac != 3)
+	{
+		fprintf(stderr, "%s: wrong arguments\n", av[0]);
+		exit(1);
+	}
+
+	if(rename(av[1], av[2]) < 0)
+	{
+		perror(av[1]);
+		exit(1);
+	}
+	exit(0);
+}
